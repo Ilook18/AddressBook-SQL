14a235f0774f9536ffd6e146aa985035db181d55
“Enter <anirudhanand984@gmail.com>
2021-12-16 16:44:44 +0530

    [Anirudh] add : Inserted values into address book table

--- AddressBookServices.sql
+++ AddressBookServices.sql
@@ -5,7 +5,16 @@ use address_book_service;
 /*UC2*/
 create table address_book(id int identity(1,1) primary key, firstname varchar(30), lastname varchar(30), address varchar(100), city varchar(50), state varchar(20), zip char(5), pno varchar(50), email varchar(50));
 
-
-
+/*UC3*/
+insert into address_book values
+('Anirudh','Anand','Mohaddipur','Gorakhpur','UP','27300','7905142214','anirudhanand984@gmail.com'),
+('Shashank','Singh','Kunraghat','Gorakhpur','UP','27301','7905142215','sash@gmail.com'),
+('Punit','Yadav','Semra','Lucknow','UP','26001','7905142216','punit@gmail.com'),
+('Pawan','Kumar','Andheri','Mumbai','Maharashtra','26000','7905142217','pawan@gmail.com'),
+('Rahul','Khanna','Sector6','Chandigarh','Punjab','25002','7905142218','rahul@gmail.com'),
+('Rohan','Malhotra','Sector7','Amritsar','Punjab','25001','7905142219','rohan@gmail.com'),
+('Raj','Sinha','Sector8','Ludhiana','Punjab','25003','7905142211','raj@gmail.com'),
+('Akshay','Rana','Sector9','Bhopal','MP','24000','7905142212','akshay@gmail.com');
+select * from address_book;
 
 
