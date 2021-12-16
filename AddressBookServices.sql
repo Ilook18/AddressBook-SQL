ed143300135e7dddde9257c521aff5922e51d23e
“Enter <anirudhanand984@gmail.com>
2021-12-16 16:26:58 +0530

    [Anirudh] add : Created Table

--- AddressBookServices.sql
+++ AddressBookServices.sql
@@ -2,6 +2,10 @@
 create database address_book_service;
 use address_book_service;
 
+/*UC2*/
+create table address_book(id int identity(1,1) primary key, firstname varchar(30), lastname varchar(30), address varchar(100), city varchar(50), state varchar(20), zip char(5), pno varchar(50), email varchar(50));
+
+
 
 
 
